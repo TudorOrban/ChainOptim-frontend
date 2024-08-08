f01b265bc424f82bde91796a0c68f151cf61cfa4
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-08-08 21:35:00 +0300

    Use Score Component for displaying factory performance scores

--- src/app/dashboard/production/components/factory-performances/factory-performances.component.ts
+++ src/app/dashboard/production/components/factory-performances/factory-performances.component.ts
@@ -1,13 +1,142 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faIndustry } from '@fortawesome/free-solid-svg-icons';
+import { Factory } from '../../models/Factory';
+import { FactoryService } from '../../services/factory.service';
+import { Organization } from '../../../organization/models/organization';
+import { FallbackManagerComponent } from '../../../../shared/fallback/components/fallback-manager/fallback-manager.component';
+import {
+    FallbackManagerService,
+    FallbackManagerState,
+} from '../../../../shared/fallback/services/fallback-manager/fallback-manager.service';
+import { ListHeaderComponent } from '../../../../shared/common/components/list-header/list-header.component';
+import { PageSelectorComponent } from '../../../../shared/search/components/page-selector/page-selector.component';
+import { SortOption } from '../../../../shared/search/models/Search';
+import { UserService } from '../../../../core/auth/services/user.service';
 import { ScoreComponent } from '../../../../shared/common/components/score/score.component';
 
 @Component({
-  selector: 'app-factory-performances',
-  standalone: true,
-  imports: [ScoreComponent],
-  templateUrl: './factory-performances.component.html',
-  styleUrl: './factory-performances.component.css'
+    selector: 'app-factory-performances',
+    standalone: true,
+    imports: [
+        CommonModule,
+        FontAwesomeModule,
+        RouterModule,
+        FallbackManagerComponent,
+        ListHeaderComponent,
+        PageSelectorComponent,
+        ScoreComponent
+    ],
+    templateUrl: './factory-performances.component.html',
+    styleUrl: './factory-performances.component.css',
 })
-export class FactoryPerformancesComponent {
+export class FactoryPerformancesComponent implements OnInit {
+    currentOrganization: Organization | undefined = undefined;
+    factories: Factory[] = [];
+    totalCount = 0;
+    fallbackManagerState: FallbackManagerState = {};
 
+    // Search params
+    searchQuery = "";
+    sortOptions: SortOption[] = [
+        { label: "Created At", value: "createdAt" },
+        { label: "Updated At", value: "updatedAt" },
+        { label: "Name", value: "name" }
+    ];
+    currentSortOption: SortOption = { label: "createdAt", value: "createdAt" };
+    ascending = false;
+    page = 1;
+    itemsPerPage = 10;
+    
+    constructor(
+        private userService: UserService,
+        private factoryService: FactoryService,
+        private fallbackManagerService: FallbackManagerService
+    ) {}
+
+    ngOnInit() {
+        // Manage fallbacks
+        this.fallbackManagerService.fallbackManagerState$.subscribe((state) => {
+            this.fallbackManagerState = state;
+        });
+        this.fallbackManagerService.updateLoading(true);
+
+        // Get current user's organization
+        this.userService
+            .getCurrentUser()
+            .subscribe({
+                next: (user) => {
+                    console.log('Current User:', user);
+                    this.currentOrganization = user?.organization;
+                    if (user && user.organization) {
+                        this.fallbackManagerService.updateNoOrganization(false);
+
+                        // Load factories
+                        this.loadFactories(this.currentOrganization?.id ?? 0);
+                    } else {
+                        this.fallbackManagerService.updateNoOrganization(true);
+                    }
+                },
+                error: (error: Error) => {
+                    this.fallbackManagerService.updateError(
+                        error.message ?? ''
+                    );
+                    this.fallbackManagerService.updateLoading(false);
+                },
+            });
+    }
+
+    private loadFactories(organizationId: number) {
+        this.fallbackManagerService.updateLoading(true);
+
+        this.factoryService
+            .getFactoriesByOrganizationIdAdvanced(organizationId, this.searchQuery, this.currentSortOption.value, this.ascending, this.page, this.itemsPerPage)
+            .subscribe({
+                next: (paginatedResults) => {
+                    this.factories = paginatedResults.results;
+                    this.totalCount = paginatedResults.totalCount;
+
+                    console.log('Factories:', this.factories);
+                    // Manage fallback state
+                    if (this.factories.length === 0) {
+                        this.fallbackManagerService.updateNoResults(true);
+                    } else {
+                        this.fallbackManagerService.updateNoResults(false);
+                    }
+                    this.fallbackManagerService.updateLoading(false);
+                },
+                error: (err: Error) => {
+                    this.fallbackManagerService.updateError(err.message ?? '');
+                    this.fallbackManagerService.updateLoading(false);
+                },
+            });
+    }
+
+    handleSearch(query: string): void {
+        if (this.searchQuery !== query) {
+            this.searchQuery = query;
+            this.page = 1; // Reset page
+            this.loadFactories(this.currentOrganization!.id);
+        }
+    }
+
+    handleSortChange(sortChange: { value: string, ascending: boolean }): void {
+        if (this.currentSortOption.value !== sortChange.value || this.ascending !== sortChange.ascending) {
+            this.currentSortOption = this.sortOptions.find((option) => option.value === sortChange.value)!;
+            this.ascending = sortChange.ascending;
+            this.page = 1; // Reset page
+            this.loadFactories(this.currentOrganization!.id);
+        }
+    }
+
+    changePage(page: number): void {
+        if (this.page !== page) {
+            this.page = page;
+            this.loadFactories(this.currentOrganization!.id);
+        }
+    }
+
+    faIndustry = faIndustry;
 }
