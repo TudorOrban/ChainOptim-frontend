1d773cdddf035577985b384b31521d8c1c85b11c
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-08-22 22:06:37 +0300

    Start fleshing out Custom Plan Component

--- src/app/core/main/components/pricing/custom-plan/custom-plan.component.ts
+++ src/app/core/main/components/pricing/custom-plan/custom-plan.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Feature } from '../../../../../shared/enums/commonEnums';
+import { PlanTier } from '../../../../../dashboard/organization/models/SubscriptionPlan';
+import { SubscriptionPlanService } from '../../../../../dashboard/organization/services/subscriptionplan.service';
 
 @Component({
   selector: 'app-custom-plan',
@@ -12,8 +15,34 @@ import { FormsModule } from '@angular/forms';
 export class CustomPlanComponent {
 
     isMonthly: boolean = true;
+    selectedPlanTier: PlanTier | undefined = PlanTier.NONE;
+
+    Feature = Feature;
+    PlanTier = PlanTier;
+
+    planService: SubscriptionPlanService;
+
+    constructor(
+        planService: SubscriptionPlanService,
+    ) {
+        this.planService = planService;
+    }
 
     switchTime(): void {
         this.isMonthly = !this.isMonthly;
+    }
+
+    selectPlanTier(planTier: PlanTier): void {
+        this.selectedPlanTier = planTier;
+    }
+    
+    // Utils
+    getObjectValues(obj: any): any[] {
+        return Object.values(obj);
+    }
+
+    decapitalize(word?: string) {
+        if (!word) return '';
+        return word.charAt(0) + word.slice(1).toLowerCase();
     }
 }
